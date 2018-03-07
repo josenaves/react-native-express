f466c503bd76b33c7b8b644dc734e30e435a215f
José Naves <josenaves@gmail.com>
2018-03-07 17:14:35 -0300

    Translate the section Imports And Exports to Brazilian Portuguese

--- src/pages/ImportsAndExports.js
+++ src/pages/ImportsAndExports.js
@@ -18,25 +18,25 @@ const exportExample = `export default React
 export {View, Text, Image}`
 
 const content = markdown(markdownOptions)`
-ES6 provides a more advanced module importing/exporting pattern than the widely used CommonJS pattern. By contrast to the old \`${'module.exports = {...}'}\`, we can now export multiple named values. Similarly, we can import multiple named values.
+ES6 nos traz um padrão mais avançado de se importar/exportar módulos do que o tão utilizado padrão CommonJS. Contrastando com o velho \`${'module.exports = {...}'}\`, agora nós podemos exportar múltiplos \`named values\`. Da mesma maneira, podemos importar múltiplos \`named values\`.
 
-There is one default export per file, and this exported value can be imported without refering to it by name. Every other import and export must be named.
+Existe apenas um export default por arquivo, e esse valor exportado pode ser importado sem termos que referenciá-lo por nome. Todos os outros imports e exports devem ser nomeados.
 
-If you attempt to import an older CommonJS module using the new import syntax, Babel will handle the difference in format automatically at runtime. An old-style module won't have the special \`__esModule\` property added by Babel upon export.
+Se você tentar importar um velho módulo CommonJS usando a nova sintaxe de import, Babel vai cuidar da diferença no formato automaticamente. Um módulo que segue o estilo antigo não terá a propriedade especial  \`__esModule\` adicionada pelo Babel ao ser exportada.
 
 <EditorTranspiler
   code=${importExample}
-  title=${'Importing'}
+  title=${'Importando'}
 />
 
-The imports in the previous example would be available if exported from the module \`react-native\` as in the next example.
+Os imports no exemplo anterior estariam disponíveis caso fossem exportados do módulo \`react-native\` como no próximo exemplo.
 
 <EditorTranspiler
   code=${exportExample}
-  title=${'Exporting'}
+  title=${'Exportando'}
 />
 
-For full details on the importing/exporting syntax, see the MDN reference for [import](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import) and [export](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export).
+Para maiores detalhes na sintaxe de importação/exportação, veja a referência no MDN para [import](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import) e [export](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export).
 `
 
 export default props => <DefaultPage {...props}>{content}</DefaultPage>
